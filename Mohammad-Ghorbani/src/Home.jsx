30e242d0460e972e4a0cdd06c73ba5af2f9c06c8
Mohamad Ghorbani <mohamad.ghorbani.official@gmail.com>
2023-10-26 00:47:29 +0330

    fix bugs

--- Mohammad-Ghorbani/src/Home.jsx
+++ Mohammad-Ghorbani/src/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
 
   return (
     <div className="box-border bg-[#34393C] w-screen h-screen pl-10 pt-5 pr-10 pb-14 text-white text-right cursor-default font-sans">
-      <div className="w-full h-full m-auto mt-5 p-2 rounded-3xl flex flex-row bg-[#202329] max-lg:relative relative">
+      <div className="w-full h-full m-auto mt-5 p-2 rounded-3xl flex flex-row bg-[#202329] max-lg:relative relative ">
         <ChatBox
           selectedContact={selectedContact}
           sender={sender}
